c83649fdd1015f4321fe5f01a7ea0af9ffb67eca
Guillem <guillem.aldeguer@gmail.com>
2015-06-09 10:57:06 +0200

    Added scale and opacity parameters

--- dist/angular-leaflet-directive-ext.d3.js
+++ dist/angular-leaflet-directive-ext.d3.js
@@ -70,10 +70,19 @@ angular.module('leaflet-directive.ext.d3.ping').directive('ping', function() {
 			// Get the leaflet scope from the parent leaflet controller
 			var leafletScope = controller.getLeafletScope();
 
-			controller.getMap().then(function(map) {
+			controller.getMap().then(function (map) {
 				// Initialize the ping layer
 				var pingLayer = L.pingLayer(leafletScope.ping.config).addTo(map);
-				leafletScope.$on(leafletScope.ping.event, function(event, pingData){
+
+				if (angular.isDefined(leafletScope.ping.radiusScale)) {
+					pingLayer.opacityScale().range(leafletScope.ping.radiusScale);
+				}
+
+				if (angular.isDefined(leafletScope.ping.opacityScale)) {
+					pingLayer.opacityScale().range(leafletScope.ping.opacityScale);
+				}
+
+				leafletScope.$on(leafletScope.ping.event, function (event, pingData){
 					pingLayer.ping(pingData);
 				});
 			});
